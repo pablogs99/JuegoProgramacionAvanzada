9efbbb9e1d4b818889fae1ef2c31aa8419acd9f7
Wei Zheng <44177038+weizheng2@users.noreply.github.com>
2020-02-06 19:50:50 +0100

    etc

--- Juego/engine/engine/ObstaculoCubo.h
+++ Juego/engine/engine/ObstaculoCubo.h
@@ -6,6 +6,8 @@ class ObstaculoCubo: public Cube
 public:
 	ObstaculoCubo(){}
 	ObstaculoCubo(Vector3D position, Color color, Vector3D speed, Vector3D orientation) :
-		Cube(position,color,speed,orientation){}
+		Cube(position,color,speed,orientation){
+		this->SetSize(2);
+	}
 };
 
--- Juego/engine/engine/ObstaculoEsfera.h
+++ Juego/engine/engine/ObstaculoEsfera.h
@@ -6,6 +6,8 @@ class ObstaculoEsfera: public Sphere
 public:
 	ObstaculoEsfera(){}
 	ObstaculoEsfera(Vector3D position, Color color, Vector3D speed,Vector3D orientation) :
-		Sphere(position,color,speed,orientation){}
+		Sphere(position,color,speed,orientation){
+		this->SetRadius(1.3);
+	}
 };
 
